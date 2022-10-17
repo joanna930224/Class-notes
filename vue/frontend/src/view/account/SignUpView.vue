3d93ad610ecea6184041eed3079ff98fba6bd6bd
ANNA <109578911+joanna930224@users.noreply.github.com>
2022-10-17 22:35:18 +0900

    SignUpView.vue

---
+++ vue/frontend/src/view/account/SignUpView.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <sign-up-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import SignUpForm from "@/components/account/SignUpForm";
+export default {
+  name: "SignUpView",
+  components: {
+    SignUpForm
+  },
+  methods: {
+    onSubmit (payload) {
+      const { email, password, city, street, addressDetail, zipcode } = payload;
+      axios.post("http://localhost:8888/member/sign-up", {
+        email, password, city, street, addressDetail, zipcode
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
