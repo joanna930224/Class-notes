75f533aec29f9cc632535487060e0d46baf94a1e
ANNA <109578911+joanna930224@users.noreply.github.com>
2022-10-18 14:09:43 +0900

    회원가입 폼 만들기 [CREATE-SIGN-UP]

---
+++ vue/frontendsrc/router/index.js
@@ -0,0 +1,168 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Test from '@/components/basics/Test.vue'
+import ComponentTestView from "@/views/basics/ComponentTestView";
+import ConcaveBoardView from "@/views/game/ConcaveBoardView";
+import VueToSpringView from "@/views/axiosTest/VueToSpringView";
+import RpgGameView from "@/views/rpg/RpgGameView";
+import DataSendTestView from "@/views/basics/DataSendTestView";
+
+import JpaBoardListView from "@/views/boards/JpaBoardListView";
+import JpaBoardRegisterView from "@/views/boards/JpaBoardRegisterView";
+import JpaBoardModifyView from "@/views/boards/JpaBoardModifyView";
+import JpaBoardReadView from "@/views/boards/JpaBoardReadView";
+
+import ProductBoardListView from "@/views/boards/products/ProductBoardListView";
+import ProductBoardRegisterView from "@/views/boards/products/ProductBoardRegisterView";
+import ProductBoardReadView from "@/views/boards/products/ProductBoardReadView";
+import ProductBoardModifyView from "@/views/boards/products/ProductBoardModifyView";
+
+import VuetifyTasteView from "@/views/vuetify/VuetifyTasteView";
+import SwiperTestView from "@/views/vuetify/SwiperTestView";
+import CalendarTestView from "@/views/vuetify/CalendarTestView";
+import ImageGalleryTestView from "@/views/vuetify/ImageGalleryTestView";
+import FileUploadView from "@/views/file/FileUploadView";
+import SignUpView from "@/views/account/SignUpView";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: Test
+  },
+  {
+    path: '/components-test',
+    name: 'ComponentTest',
+    component: ComponentTestView
+  },
+  {
+    path: '/concave-board-game',
+    name: 'ConcaveBoardView',
+    component: ConcaveBoardView
+  },
+  {
+    path: '/vue2spring',
+    name: 'VueToSpringView',
+    component: VueToSpringView
+  },
+  {
+    path: '/rpg-game',
+    name: 'RpgGameView',
+    component: RpgGameView
+  },
+  {
+    path: '/vue2spring-data-send-test',
+    name: 'DataSendTestView',
+    component: DataSendTestView
+  },
+
+
+  {
+    path: '/board-list',
+    name: 'JpaBoardListView',
+    component: JpaBoardListView
+  },
+  {
+    path: '/board-register',
+    name: 'JpaBoardRegisterView',
+    component: JpaBoardRegisterView
+  },
+  {
+    path: '/board-read/:boardNo',
+    name: 'JpaBoardReadView',
+    components: {
+      default: JpaBoardReadView
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/board-modify/:boardNo',
+    name: 'JpaBoardModifyView',
+    components: {
+      default: JpaBoardModifyView
+    },
+    props: {
+      default: true
+    }
+  },
+
+
+  {
+    path: '/product-board-list',
+    name: 'ProductBoardListView',
+    component: ProductBoardListView
+  },
+  {
+    path: '/product-board-register',
+    name: 'ProductBoardRegisterView',
+    component: ProductBoardRegisterView
+  },
+  {
+    path: '/product-board-read/:productNo',
+    name: 'ProductBoardReadView',
+    components: {
+      default: ProductBoardReadView
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/product-board-modify/:productNo',
+    name: 'ProductBoardModifyView',
+    components: {
+      default: ProductBoardModifyView
+    },
+    props: {
+      default: true
+    }
+  },
+
+  {
+    path: '/vuetify-taste',
+    name: 'VuetifyTasteView',
+    component: VuetifyTasteView
+  },{
+    path: '/swiper-test',
+    name: 'SwiperTestView',
+    component: SwiperTestView
+  },
+  {
+    path: '/calendar-test',
+    name: 'CalendarTestView',
+    component: CalendarTestView
+  },
+  {
+    path: '/image-gallery-test',
+    name: 'ImageGalleryTestView',
+    component: ImageGalleryTestView
+  },
+  {
+    path: '/file-upload-test',
+    name: 'FileUploadView',
+    component: FileUploadView
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUpView',
+    component: SignUpView
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+export default router
